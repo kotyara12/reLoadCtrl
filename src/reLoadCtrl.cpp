74516d46966a4298465c7080eee20766a7148ff6
kotyara12 <kotyara12@users.noreply.github.com>
2022-01-19 22:42:01 +0300

    Optimization JSON generation

--- src/reLoadCtrl.cpp
+++ src/reLoadCtrl.cpp
@@ -217,30 +217,27 @@ bool rLoadController::mqttPublish(bool forced)
 
 char* rLoadController::getTimestampsJSON()
 {
-  char* _json_time = nullptr;
-  char* _json_time_on = nullptr;
-  char* _json_time_off = nullptr;
-  
-  if (_last_on == 0) {
-    _json_time_on = malloc_stringf(CONFIG_LOADCTRL_TIMESTAMP_NULL);
+  struct tm _ti_on;
+  char _time_on[CONFIG_LOADCTRL_TIMESTAMP_BUF_SIZE];
+  memset(&_time_on, 0, sizeof(_time_on));
+  if (_last_on > 0) {
+    localtime_r(&_last_on, &_ti_on);
+    strftime(_time_on, sizeof(_time_on), CONFIG_LOADCTRL_TIMESTAMP_FORMAT, &_ti_on);
   } else {
-    _json_time_on = malloc_timestr(CONFIG_FORMAT_TIMESTAMP_L, _last_on);
+    strcpy(_time_on, CONFIG_FORMAT_EMPTY_DATETIME);
   };
-  
-  if (_last_off == 0) {
-    _json_time_off = malloc_stringf(CONFIG_LOADCTRL_TIMESTAMP_NULL);
+
+  struct tm _ti_off;
+  char _time_off[CONFIG_LOADCTRL_TIMESTAMP_BUF_SIZE];
+  memset(&_time_off, 0, sizeof(_time_off));
+  if (_last_off > 0) {
+    localtime_r(&_last_off, &_ti_off);
+    strftime(_time_off, sizeof(_time_off), CONFIG_LOADCTRL_TIMESTAMP_FORMAT, &_ti_off);
   } else {
-    _json_time_off = malloc_timestr(CONFIG_FORMAT_TIMESTAMP_L, _last_off);
+    strcpy(_time_off, CONFIG_FORMAT_EMPTY_DATETIME);
   };
 
-  if ((_json_time_on) && (_json_time_off)) {
-    _json_time = malloc_stringf("{\"" CONFIG_LOADCTRL_ON "\":\"%s\",\"" CONFIG_LOADCTRL_OFF "\":\"%s\"}", _json_time_on, _json_time_off);
-  };
-
-  if (_json_time_on) free(_json_time_on);
-  if (_json_time_off) free(_json_time_off);
-
-  return _json_time;
+  return malloc_stringf("{\"" CONFIG_LOADCTRL_ON "\":\"%s\",\"" CONFIG_LOADCTRL_OFF "\":\"%s\"}", _time_on, _time_off);
 }
 
 char* rLoadController::getCountersJSON()
